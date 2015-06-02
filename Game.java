1ef9fc7385b70eb441a389f0c70a4f9f88eb5267
mcgrammar86 <mikecarlson86@gmail.com>
2015-06-02 16:23:48 -0500

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,26 @@
+public class Game extends JFrame{
+	
+	public static void main(String[] args){
+		private Level mLevel; //eventually to be converted to Array of Level objects
+		// use an ArrayDeque to store spaceship location history
+		
+	//load level, hardcode one or two Bodies for now
+	
+	
+	//wait for click to arm launch
+	
+	//using a do/while 
+	do {
+		//get mouse position
+		//show arrow from spaceship to mouse location
+		
+		if (clickIsReleased()){
+			spaceship.launch(mouseX-spaceship.getX()
+		
+	} (while !isLaunched());
+	
+	}
+	public void displayGame(Level level){
+		//parse out objects
+	}
+}
