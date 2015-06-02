eee99ea1c2e04e5237e3d55eb0bbbd5e4f0ce592
mcgrammar86 <mikecarlson86@gmail.com>
2015-06-02 16:24:42 -0500

    Create Body.java

---
+++ Body.java
@@ -0,0 +1,10 @@
+public class Body{
+	public double mX;
+	public double mY;
+	private double mGravity;
+	private double mRadius;
+	private int mIconId;
+	public double[2][3] updateParameter;
+	
+	//generate getters/setters
+}
